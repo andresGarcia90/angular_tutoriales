eb47e29e76c79d46d0a590c872da0c5bce36a9b8
andresGarcia90 <garcia.andi90@gmail.com>
2019-12-13 18:40:09 -0300

    Primer agregado de Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
+import { HeroesService } from './services/heroes.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeroesComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    APP_ROUTING,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
